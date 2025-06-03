f36e44410d04d95cbcc3c78b3fbd1284270af179
TheSkylerDev <kesch.inno14@gmail.com>
2025-06-03 15:12:00 +0200

    Add main.cppwith directory 0.1.0-mvp

---
+++ 0.1.0-mvp/main.cpp
@@ -0,0 +1,102 @@
+//
+// Created by i.nikiforov on 03.06.2025.
+//
+#include <iostream>
+#include <queue>
+#include <string>
+#include <format>
+#include <sstream>
+
+//GLOBAL: ANSI escape codes for text formatting
+const std::string FG_RESET  = "\033[39m";
+const std::string FG_RED    = "\033[31m"; // ERROR
+const std::string FG_YELLOW = "\033[33m"; // WARN/STATUS
+const std::string FG_GREEN  = "\033[32m"; // OK
+const std::string FG_CYAN   = "\033[36m"; // INFO
+const std::string FG_BLACK  = "\033[30m"; // CONTRAST
+const std::string BOLD      = "\033[1m";
+
+//GLOBAL: ANSI escape codes for Backgrounds
+const std::string BG_BLUE   = "\033[48;5;33m";
+const std::string BG_WHITE  = "\033[48;5;255m"; // OUTLINE
+const std::string BG_RESET  = "\033[49m";
+
+//UNIVERSAL RESET
+const std::string RESET = "\033[0m";
+
+//GLOBAL: queues
+std::queue<int> request_queue;
+std::queue<int> floor_queue;
+
+//External elevator call menu
+void call_menu(){
+    std::cout << std::format("{}{}{}=========| Call Elevator |========={}\n", BG_WHITE, FG_BLACK, BOLD, RESET);
+    std::cout << std::format("{}{}{}|{}                                 {}|{}\n", BG_WHITE, FG_BLACK, BOLD, BG_BLUE, BG_WHITE, RESET);
+    std::cout << std::format("{}{}{}|{}           {}[1] - ^ UP ^          {}{}|{}\n", BG_WHITE, FG_BLACK, BOLD, BG_BLUE, FG_CYAN, BG_WHITE, FG_BLACK, RESET);
+    std::cout << std::format("{}{}{}|{}           {}[2] - v DOWN v        {}{}|{}\n", BG_WHITE, FG_BLACK, BOLD, BG_BLUE, FG_CYAN, BG_WHITE, FG_BLACK, RESET);
+    std::cout << std::format("{}{}{}|{}                                 {}|{}\n", BG_WHITE, FG_BLACK, BOLD, BG_BLUE, BG_WHITE, RESET);
+    std::cout << std::format("{}{}{}==================================={}\n", BG_WHITE, FG_BLACK, BOLD, RESET);
+};
+
+//floor selection
+void floor_select(){
+    std::cout << std::format("{}{}{}=========| Select Floor |========={}\n", BG_WHITE, FG_BLACK, BOLD, RESET);
+    std::cout << std::format("{}{}{}|{}                                 {}|{}\n", BG_WHITE, FG_BLACK, BOLD, BG_BLUE, BG_WHITE, RESET);
+    std::cout << std::format("{}{}{}|{}        {}[1] - 1st Floor          {}{}|{}\n", BG_WHITE, FG_BLACK, BOLD, BG_BLUE, FG_YELLOW, BG_WHITE, FG_BLACK, RESET);
+    std::cout << std::format("{}{}{}|{}        {}[2] - 2nd Floor          {}{}|{}\n", BG_WHITE, FG_BLACK, BOLD, BG_BLUE, FG_YELLOW, BG_WHITE, FG_BLACK, RESET);
+    std::cout << std::format("{}{}{}|{}        {}[3] - 3rd Floor          {}{}|{}\n", BG_WHITE, FG_BLACK, BOLD, BG_BLUE, FG_YELLOW, BG_WHITE, FG_BLACK, RESET);
+    std::cout << std::format("{}{}{}|{}        {}[4] - 4th Floor          {}{}|{}\n", BG_WHITE, FG_BLACK, BOLD, BG_BLUE, FG_YELLOW, BG_WHITE, FG_BLACK, RESET);
+    std::cout << std::format("{}{}{}|{}        {}[5] - 5th Floor        {}{}|{}\n", BG_WHITE, FG_BLACK, BOLD, BG_BLUE, FG_YELLOW, BG_WHITE, FG_BLACK, RESET);
+    std::cout << std::format("{}{}{}|{}                                 {}|{}\n", BG_WHITE, FG_BLACK, BOLD, BG_BLUE, BG_WHITE, RESET);
+    std::cout << std::format("{}{}{}==================================={}\n", BG_WHITE, FG_BLACK, BOLD, RESET);
+};
+
+//Elevator movement
+/*int elevator_movement(int dest){}*/ //TO BE IMPLEMENTED SOON
+
+// Helper function to convert queue contents to string. DEBUG ONLY
+template<typename T>
+std::string queue_to_string(std::queue<T> q) {
+    std::stringstream ss;
+    ss << "[";
+    while (!q.empty()) {
+        ss << q.front();
+        q.pop();
+        if (!q.empty()) ss << ", ";
+    }
+    ss << "]";
+    return ss.str();
+}
+
+//MAIN
+int main() {
+    try {
+        int mode = 0;
+        std::cin >> mode;
+        while (true) {
+            switch (mode) {
+                case 1:
+                    call_menu();
+                    int direction;
+                    std::cin >> direction;
+                    request_queue.push(direction);
+                    std::cout << std::format("{}DEBUG: Request pushed to queue: {}{}{}\n", FG_YELLOW, BG_WHITE, queue_to_string(request_queue), RESET);
+                    break;
+
+                case 2:
+                    floor_select();
+                    int floor;
+                    std::cin >> floor;
+                    floor_queue.push(floor);
+                    std::cout << std::format("{}DEBUG: Floor pushed to queue: {}{}{}\n", FG_YELLOW, BG_WHITE, queue_to_string(floor_queue), RESET);
+
+                default:
+                    std::cout << std::format("{}{}{}Invalid mode{}\n", BG_WHITE, FG_RED, BOLD, RESET);
+            }
+        }
+    } catch (const std::exception& e ) {
+        std::cout << e.what() << '\n';
+        throw std::runtime_error("Invalid mode");
+    }
+    return 0;
+}
